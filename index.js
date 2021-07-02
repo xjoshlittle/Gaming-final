f4a0b91d5d4f31fcadeabacb83ebd2f046544f6c
Josh Little <jlittle10@cnm.edu>
2021-07-02 10:05:57 -0600

    mongo, express, backend, setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express ();
+const mongoose =require("mongoose");
+const dotenv =require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users")
+
+
+dotenv.config();
+//keys are inside the env file
+mongoose.connect('process.env.MONGO_URL', {useNewUrlParser: true, useUnifiedTopology:true}, () => {
+    console.log("connected to mongoDB")
+});
+
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users", userRoute)
+
+
+
+
+app.listen(8800,() => {
+    console.log("back end server is ready")
+})
